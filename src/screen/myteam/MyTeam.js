52b1c2010d38d44702c3674af1d6ad178802f063
jakibenz <43321959+jakibenz@users.noreply.github.com>
2019-11-12 12:28:18 +0800

    Fixed my team height

--- src/screen/myteam/MyTeam.js
+++ src/screen/myteam/MyTeam.js
@@ -12,7 +12,7 @@ const MyTeam = ({ teamList, dispatch }) => {
     }
 
     return (
-        <div className='tc pt6 pt6-ns pr5 ma0 w-30' style={{ overflowY: 'auto', scrollBehavior: 'smooth', border: '3px hidden black', height: '80vh' }}>
+        <div className='tc pr5 ma0 w-30 relative' style={{ overflowY: 'auto', scrollBehavior: 'smooth', border: '3px hidden black', height: '80vh', top: 145 }}>
                 <h2 className='f6 f2-ns'>My Team</h2>
                 {teamList.map((element) => {
                     return <TeamCard key={element.id} teamDetails={element} onDelete={(e) => handleDelete(e)} />
